2f00985f80920a630c964e1c12fda226c00ee5de
Liana Mancini <liana.mancini@gmail.com>
2018-03-07 13:21:56 -0500

    built static version of a sample question

--- src/App.js
+++ src/App.js
@@ -10,12 +10,51 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <GameBoard questions={QUESTIONS} time={TIME} />
       </div>
     );
   }
 }
+
+class Answer extends Component {
+  render() {
+    return(
+      <div>
+        {this.props.item}
+      </div>
+    )
+  }
+}
+
+class PlayArea extends Component {
+  render(){
+    var answers = [];
+    for (var i = 0; i < this.props.questions.answers.length; i++){
+      answers.push(
+        <Answer item={this.props.questions.answers[i]} />
+      )
+    }
+    return(
+      <div>
+        <p>Time: {this.props.time}</p>
+        <p>{this.props.questions.question}</p>
+        <div className="answers">{answers}</div>
+      </div>
+    )
+  }
+}
+
+class GameBoard extends Component {
+  render(){
+    return(
+      <div>
+        <PlayArea time={this.props.time} questions={this.props.questions} />
+      </div>
+    )
+  }
+}
+
+const QUESTIONS = {question: "What is my name?", answers: ["Liana", "Bob", "Sally", "Jo"], correctAnswer: "Liana"};
+const TIME = 30;
 
 export default App;
