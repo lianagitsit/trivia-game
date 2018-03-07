1bdf258d4a718a6af257e7df310d4ae53369b0b7
Liana Mancini <liana.mancini@gmail.com>
2018-03-07 15:54:38 -0500

    added timer, refactored to merge PlayArea into GameBoard component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const QUESTIONS = {question: "What is my name?", answers: ["Liana", "Bob", "Sally", "Jo"], correctAnswer: "Liana"};
+// const TIME = 30;
+
 class App extends Component {
   render() {
     return (
@@ -10,33 +13,55 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <GameBoard questions={QUESTIONS} time={TIME} />
+        <GameBoard questions={QUESTIONS} />
       </div>
     );
   }
 }
 
-class Answer extends Component {
-  render() {
-    return(
-      <div>
-        {this.props.item}
-      </div>
-    )
+class GameBoard extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      timeToAnswer: 5
+    }
   }
-}
 
-class PlayArea extends Component {
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    console.log("Time: " + this.state.timeToAnswer);
+    this.setState({
+      timeToAnswer: this.state.timeToAnswer - 1
+    });
+    if(this.state.timeToAnswer === 0){
+      clearInterval(this.timerID);
+    }
+  }
+
   render(){
+
     var answers = [];
     for (var i = 0; i < this.props.questions.answers.length; i++){
       answers.push(
-        <Answer item={this.props.questions.answers[i]} />
+        <Answer item={this.props.questions.answers[i]} key={i} />
       )
     }
+
     return(
       <div>
-        <p>Time: {this.props.time}</p>
+        <h1>A Trivia Game</h1>
+        {/* <PlayArea time={this.state.timeToAnswer} questions={this.props.questions} /> */}
+        <p>Time: {this.state.timeToAnswer}</p>
         <p>{this.props.questions.question}</p>
         <div className="answers">{answers}</div>
       </div>
@@ -44,17 +69,42 @@ class PlayArea extends Component {
   }
 }
 
-class GameBoard extends Component {
-  render(){
+// class PlayArea extends Component {
+//   render(){
+//     var answers = [];
+//     for (var i = 0; i < this.props.questions.answers.length; i++){
+//       answers.push(
+//         <Answer item={this.props.questions.answers[i]} key={i} />
+//       )
+//     }
+
+//     return(
+//       <div>
+//         <p>Time: {this.props.time}</p>
+//         <p>{this.props.questions.question}</p>
+//         <div className="answers">{answers}</div>
+//       </div>
+//     )
+//   }
+// }
+
+class Answer extends Component {
+  constructor(props){
+    super(props);
+    this.onAnswerClick = this.onAnswerClick.bind(this);
+  }
+
+  onAnswerClick(){
+    console.log("clicked " + this.props.item);
+  }
+
+  render() {
     return(
-      <div>
-        <PlayArea time={this.props.time} questions={this.props.questions} />
+      <div className="answer" onClick={this.onAnswerClick}>
+        {this.props.item}
       </div>
     )
   }
 }
-
-const QUESTIONS = {question: "What is my name?", answers: ["Liana", "Bob", "Sally", "Jo"], correctAnswer: "Liana"};
-const TIME = 30;
 
 export default App;
