47170486f743692eaa4d887f28d74f22b9c86ff7
Liana Mancini <liana.mancini@gmail.com>
2018-03-07 21:43:11 -0500

    built basic game infrastructure, extracted Question component and added Outcome component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 const QUESTIONS = {question: "What is my name?", answers: ["Liana", "Bob", "Sally", "Jo"], correctAnswer: "Liana"};
-// const TIME = 30;
 
 class App extends Component {
   render() {
@@ -13,7 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <GameBoard questions={QUESTIONS} />
+        <GameBoard questions={QUESTIONS} outcomes={OUTCOMES} />
       </div>
     );
   }
@@ -24,9 +23,7 @@ class GameBoard extends Component {
     super(props);
     this.state = {
       timeToAnswer: 5,
-      // guess: ''
     }
-
     this.handleGuess = this.handleGuess.bind(this);
   }
 
@@ -42,7 +39,6 @@ class GameBoard extends Component {
   }
 
   tick() {
-    // console.log("Time: " + this.state.timeToAnswer);
     this.setState({
       timeToAnswer: this.state.timeToAnswer - 1
     });
@@ -59,14 +55,23 @@ class GameBoard extends Component {
 
   render(){
     console.log("Guess: " + this.state.guess);
+    console.log(this.state.timeToAnswer);
 
-    var answers = [];
-    for (var i = 0; i < this.props.questions.answers.length; i++){
-      answers.push(
-        <Answer 
-          item={this.props.questions.answers[i]} 
-          key={i}
-          onUserGuess={this.handleGuess}
+    var display;
+    if (!this.state.guess && this.state.timeToAnswer > 0){
+      display = (
+        <Question 
+          questions={this.props.questions} 
+          onUserGuess={this.handleGuess} 
+        />
+      )
+    } else if (this.state.guess || this.state.timeToAnswer === 0){
+      clearInterval(this.timerID);
+      display = (
+        <Outcome 
+          guess={this.state.guess} 
+          correctAnswer={this.props.questions.correctAnswer} 
+          timeToAnswer={this.state.timeToAnswer} 
         />
       )
     }
@@ -75,8 +80,52 @@ class GameBoard extends Component {
       <div>
         <h1>A Trivia Game</h1>
         <p>Time: {this.state.timeToAnswer}</p>
+        {display}
+      </div>
+    )
+  }
+}
+
+class Outcome extends Component {
+  render(){
+    var message;
+    var correctAnswer = (<p>The correct answer is {this.props.correctAnswer}!</p>);
+    if (this.props.timeToAnswer === 0){
+      message = (<h3>Time's Up!</h3>);
+    } else if (this.props.guess === this.props.correctAnswer){
+      message = (<h3>Correct!</h3>);
+      correctAnswer = false;
+    } else {
+      message = (<h3>Nope!</h3>);
+    }
+
+    return (
+      <div>
+        {message}
+        {correctAnswer}
+      </div>
+    )
+  }
+}
+
+// Change this to a simple function since it doesn't have state?
+class Question extends Component {
+  render() {
+    var answers = [];
+    for (var i = 0; i < this.props.questions.answers.length; i++){
+      answers.push(
+        <Answer 
+          item={this.props.questions.answers[i]} 
+          key={i}
+          onUserGuess={this.props.onUserGuess}
+        />
+      )
+    }
+
+    return (
+      <div>
         <p>{this.props.questions.question}</p>
-        <div className="answers">{answers}</div>
+        <div className="Answers">{answers}</div>
       </div>
     )
   }
@@ -95,7 +144,7 @@ class Answer extends Component {
 
   render() {
     return(
-      <div className="answer" onClick={this.handleGuess}>
+      <div className="Answer" onClick={this.handleGuess}>
         {this.props.item}
       </div>
     )
